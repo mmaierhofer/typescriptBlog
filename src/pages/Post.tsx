30bb964c7f5f6890c84cd2e2ea1b3ed46fa542a1
maierhoferma81177@th-nuernberg.de <maierhoferma81177@th-nuernberg.de>
2022-01-09 18:24:33 +0100

    fix unused imports

--- src/pages/Post.tsx
+++ src/pages/Post.tsx
@@ -1,5 +1,4 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
