49088dcd5431eda7bec904c6bf3c65c2608ada47
maierhoferma81177@th-nuernberg.de <mmaierhofer@outlook.de>
2022-01-22 14:00:29 +0100

    reverse code example

--- src/components/CodeBlock.js
+++ src/components/CodeBlock.js
@@ -5,7 +5,7 @@ import { materialDark } from "react-syntax-highlighter/dist/esm/styles/prism";
 export default function CodeBlock() {
   const codeString = `const memoizedValue = useMemo(() => 
     computeExpensiveValue(a, b), [a, b]
-    )`;
+    );`;
   return (
     <div className="w-full p-10 text-xs">
       <SyntaxHighlighter language="javascript" style={materialDark}>
