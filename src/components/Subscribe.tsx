0d1da78f6aabe674263566695074de888a4f6fc5
maierhoferma81177@th-nuernberg.de <mmaierhofer@outlook.de>
2022-04-04 20:03:04 +0200

    update tailwind and minor design changes2

--- src/components/Subscribe.tsx
+++ src/components/Subscribe.tsx
@@ -10,11 +10,11 @@ export default function Subscribe() {
   }
   return (
     <>
-      <div className="w-full h-48 flex justify-center items-center mt-5 fixed bottom-5 animate-bounce-start z-50 lg:w-48">
+      <div className="md:w-1/2 w-full h-48 flex justify-center items-center mt-5 fixed bottom-5 animate-bounce-start z-50 ">
         <div className="bg-slate-800 w-11/12 h-full rounded-3xl flex flex-col items-center justify-around pb-4">
           <div className="h-6"></div>
           <div
-            className="absolute top-2 right-6 rounded-full border-gray-400 border h-8 w-8 text-white flex justify-center items-center md:right-12"
+            className="absolute top-2 right-6 rounded-full border-gray-400 border h-8 w-8 text-white flex justify-center items-center md:right-12 cursor-pointer"
             onClick={() => setIsVisible((prevIsVisible) => !prevIsVisible)}
           >
             <FontAwesomeIcon icon={faTimes} color="lightgray" />
@@ -25,7 +25,7 @@ export default function Subscribe() {
           <div className="w-5/6 mt-2 text-white">
             Subscribe to React Butter's <br /> Newsletter
           </div>
-          <div className="w-5/6 bg-orange-200 h-8 rounded-full flex items-center justify-center color-slate-800 font-bold">
+          <div className="w-5/6 bg-orange-200 h-8 rounded-full flex items-center justify-center color-slate-800 font-bold cursor-pointer">
             Subscribe
           </div>
         </div>
--- src/pages/Post.tsx
+++ src/pages/Post.tsx
@@ -8,28 +8,30 @@ import TextBlock from "../components/TextBlock";
 export default function Post() {
   return (
     <>
-      <div className="w-full flex flex-col justify-center mt-5">
-        <div className="w-full">
-          <div
-            className="w-full h-24 bg-slate-600 flex items-center justify-start"
-            style={{
-              backgroundImage:
-                "url('//a.storyblok.com/f/139174/741x417/8f95d4abc2/usememo.png')",
-              backgroundSize: "cover",
-            }}
-          >
-            <div className="w-10 ml-10 h-10 rounded-full shadow-inner-xl text-slate-80 flex justify-center items-center">
-              <Link to="/">
-                <FontAwesomeIcon icon={faChevronLeft} />
-              </Link>
+      <div className="flex w-full flex-row items-center">
+        <div className="w-1/2 flex flex-col justify-center mt-5">
+          <div className="w-full">
+            <div
+              className="w-full h-24 bg-slate-600 flex items-center justify-start"
+              style={{
+                backgroundImage:
+                  "url('//a.storyblok.com/f/139174/741x417/8f95d4abc2/usememo.png')",
+                backgroundSize: "cover",
+              }}
+            >
+              <div className="w-10 ml-10 h-10 rounded-full shadow-inner-xl text-slate-80 flex justify-center items-center">
+                <Link to="/">
+                  <FontAwesomeIcon icon={faChevronLeft} />
+                </Link>
+              </div>
             </div>
-          </div>
 
-          <div className="mt-5 font-bold text-xl pl-10">
-            Why you shouldn't overuse the useMemo-Hook
+            <div className="mt-5 font-bold text-xl pl-10">
+              Why you shouldn't overuse the useMemo-Hook
+            </div>
+            <TextBlock text={""} />
+            <CodeBlock />
           </div>
-          <TextBlock text={""} />
-          <CodeBlock />
         </div>
       </div>
     </>
